c1c539b2a3853dce3292c08003b83dd69956e2b5
Robert Kruszewski <robertk@palantir.com>
2018-08-06 00:45:41 -0400

    add source set test

--- gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineCheckstyleTest.groovy
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineCheckstyleTest.groovy
@@ -18,9 +18,13 @@ package com.palantir.baseline
 
 import com.palantir.baseline.plugins.BaselineCheckstyle
 import com.palantir.baseline.plugins.BaselineEclipse
+import nebula.test.multiproject.MultiProjectIntegrationHelper
 import org.gradle.api.Project
+import org.gradle.api.plugins.quality.Checkstyle
 import org.gradle.api.plugins.quality.CheckstylePlugin
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 
 class BaselineCheckstyleTest extends Specification {
@@ -40,6 +44,21 @@ class BaselineCheckstyleTest extends Specification {
     def checkstylePluginApplied() {
         expect:
         project.plugins.hasPlugin(CheckstylePlugin.class)
+    }
+
+    def includesResources() {
+        def file = new File(project.projectDir, 'src/test/resources/checkstyle.xml')
+        file.getParentFile().mkdirs()
+        when:
+        file << '''
+        <?xml version="1.0"?>
+        '''.stripIndent()
+
+        then:
+        def tasks = project.tasks.withType(Checkstyle.class)
+        for (Checkstyle task : tasks) {
+            assert task.getSource().getFiles().contains(file)
+        }
     }
 
     def appliesEclipseNatures() {
